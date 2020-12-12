8c7366fc0a7bfb99b38d3b09285f505ff1362679
AdahMilly <oamildred@gmail.com>
2020-12-12 11:49:14 +0300

    add controllers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const db = require("./db/db")
 const bodyParser = require("body-parser")
+const { getAllTodos, createTodos, getATodo, deleteATodo } = require("./controllers")
 
 const app = express();
 
@@ -9,76 +10,16 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //create todos
-app.post('/api/v1/todos', (req, res) => {
-    if(!req.body.title) {
-      return res.status(400).send({
-        success: 'false',
-        message: 'title is required'
-      })
-    } else if(!req.body.description) {
-      return res.status(400).send({
-        success: 'false',
-        message: 'description is required'
-      })
-    }
-   const todo = {
-     id: db.length + 1,
-     title: req.body.title,
-     description: req.body.description
-   }
-   db.push(todo);
-   return res.status(201).send({
-     success: 'true',
-     message: 'todo added successfully',
-     todo
-   })
-  })
+app.post('/api/v1/todos', createTodos)
 
 //get all todos
-app.get('/api/v1/todos', (req,res) => {
-    res.status(200).send({
-        success: "true",
-        message: "todos retrieved successfully",
-        todos: db
-    })
-})
+app.get('/api/v1/todos', getAllTodos)
 
 //get a single todo
-app.get('/api/v1/todos/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10)
-    db.map((todo) => {
-      if (todo.id === id) {
-        return res.status(200).send({
-          success: 'true',
-          message: 'todo retrieved successfully',
-          todo,
-        })
-      } 
-  })
-   return res.status(404).send({
-     success: 'false',
-     message: 'todo does not exist',
-    })
-  })
+app.get('/api/v1/todos/:id', getATodo)
 
-  //delete a todo
-  app.delete('/api/v1/todos/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10)
-  
-    db.map((todo, index) => {
-      if (todo.id === id) {
-         db.splice(index, 1);
-         return res.status(200).send({
-           success: 'true',
-           message: 'Todo deleted successfuly',
-         })
-      }
-    })
-    return res.status(404).send({
-        success: 'false',
-        message: 'todo not found',
-    })  
-  })
+//delete a todo
+app.delete('/api/v1/todos/:id', deleteATodo)
 
 //listen to port
 const PORT = process.env.port || 5000
---
+++ controllers.js
@@ -0,0 +1,71 @@
+const db = require("./db/db")
+
+const getAllTodos = (req,res) => {
+    res.status(200).send({
+        success: "true",
+        message: "todos retrieved successfully",
+        todos: db
+    })
+}
+
+const createTodos = (req, res) => {
+    if(!req.body.title) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'title is required'
+      })
+    } else if(!req.body.description) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'description is required'
+      })
+    }
+   const todo = {
+     id: db.length + 1,
+     title: req.body.title,
+     description: req.body.description
+   }
+   db.push(todo);
+   return res.status(201).send({
+     success: 'true',
+     message: 'todo added successfully',
+     todo
+   })
+  }
+
+  const getATodo = (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    db.map((todo) => {
+      if (todo.id === id) {
+        return res.status(200).send({
+          success: 'true',
+          message: 'todo retrieved successfully',
+          todo,
+        })
+      } 
+  })
+   return res.status(404).send({
+     success: 'false',
+     message: 'todo does not exist',
+    })
+  }
+
+  const deleteATodo = (req, res) => {
+    const id = parseInt(req.params.id, 10)
+  
+    db.map((todo, index) => {
+      if (todo.id === id) {
+         db.splice(index, 1);
+         return res.status(200).send({
+           success: 'true',
+           message: 'Todo deleted successfuly',
+         })
+      }
+    })
+    return res.status(404).send({
+        success: 'false',
+        message: 'todo not found',
+    })  
+  }
+
+module.exports = {getAllTodos, createTodos , getATodo, deleteATodo}
